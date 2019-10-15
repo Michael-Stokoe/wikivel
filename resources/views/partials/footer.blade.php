bc319c4c0c073bf921fcea7fbea9317d9ce16af8
Michael Stokoe <mstokoe0990@gmail.com>
2019-10-15 09:20:27 +0100

    Update footer.blade.php

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class='w-full text-center text-white border-t border-white bg-blue-900 p-4'>
-    <p class="text-sm text-muted">Copyright © 2020 | Michael Stokoe Web Development</p>
+    <p class="text-sm text-muted">Powered by <a href="https://github.com/Stokoe0990/wikivel" target="_blank">WikiVel</a></p>
 </footer>
