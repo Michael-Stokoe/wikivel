041836b4bb9a9d1c9291c9f9575efbd2ed35e327
Michael Stokoe <mstokoe0990@Gmail.com>
2019-10-15 20:49:32 +0100

    added default display picture

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('display_picture')->nullable();
+            $table->string('display_picture')->default('default.png');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
